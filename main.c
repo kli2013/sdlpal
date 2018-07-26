cda5bbe25e9a6734db5d12bc580ec97f5c60562a
Pal Lockheart <ex@palx.org>
2018-07-26 15:36:48 +0800

    UWP relative fixes: file picker, relative reference, bugfixes etc

--- main.c
+++ main.c
@@ -58,6 +58,9 @@ PAL_Init(
 --*/
 {
    int           e;
+#if PAL_HAS_GIT_REVISION
+   UTIL_LogOutput(LOGLEVEL_DEBUG, "SDLPal build revision: %s\n", PAL_GIT_REVISION);
+#endif
 
    //
    // Initialize subsystems.
@@ -113,9 +116,6 @@ PAL_Init(
 	   ""
 #endif
    ));
-#if PAL_HAS_GIT_REVISION
-   UTIL_LogOutput(LOGLEVEL_DEBUG, "SDLPal build revision: %s\n", PAL_GIT_REVISION);
-#endif
 }
 
 VOID
